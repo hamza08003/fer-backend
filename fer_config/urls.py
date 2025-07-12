81815f0365d21954f1e6f0f5c88adfc0976d5ad2
Hamza <hamza2082003@gmail.com>
2025-07-12 09:27:35 +0500

    update root URL path for cleaner routing

--- fer_config/urls.py
+++ fer_config/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fer/auth/', include('fer_auth.urls')),
+    path('', include('fer_auth.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
